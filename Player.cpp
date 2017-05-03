0bccd0f7b8ecf96bc2953b6dd357ac90c8ef26a5
tito-kimbo <eduriver@ucm.es>
2017-05-03 12:52:39 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,13 +3,38 @@
 Player::Player(const short int & l, const long long int & s)
 {
 	lives = l;
-	score = s;
+}
+
+void Player::SetPosition(const int & x,const int & y)
+{
+	position[0] = x;
+	position[1] = y;
+}
+
+void Player::SetLives(const short int & l)
+{
+	lives = l;
+}
+
+void Player::MovePlayer(const tTecla & dir, tGame_Element **map)
+{
+	
+}
+
+int Player::ReturnLives() const
+{
+	return lives;
+}
+
+int Player::GetPositionX() const
+{
+	return position[0];
+}
+
+int Player::GetPositionY() const
+{
+	return position[1];
 }
 
 Player::~Player()
 {}
-
-const int Player::ReturnLives()
-{
-	return lives;
-}
