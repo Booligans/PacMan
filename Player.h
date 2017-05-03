5dd7b3289310e7cb5a0d42ee044ad18566efceb6
tito-kimbo <eduriver@ucm.es>
2017-05-03 12:53:09 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,16 +1,33 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include "Level_Load.h"
+
+typedef enum tTecla { UP, DOWN, LEFT, RIGHT, EXIT};
 
 class Player
 {
 	public:
 		Player(const short int & l, const long long int & s);
 
-		const int ReturnLives();
+		void SetPosition(const int & x,const int & y);
+		void SetLives(const short int & l);
+		void MovePlayer(const tTecla & dir, tGame_Element **map);
+
+		int ReturnLives() const;
+		int GetPositionX() const;
+		int GetPositionY() const;
 
 		~Player();
 	private:
 		short int lives;
+		int position[2];
 };
+
+tTecla leerTecla()
+{
+	tTecla aux;
+	//Aquí se lee del input organizado por el motor
+	return aux;
+}
 
 #endif
