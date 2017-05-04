06f9b72c50c4548412fbbf652f0c8fd05d5986e7
tito-kimbo <eduriver@ucm.es>
2017-05-04 10:51:10 +0200

    Update Enemies.cpp

--- Enemies.cpp
+++ Enemies.cpp
@@ -2,15 +2,49 @@
 
 Enemy::Enemy()
 {
-
+	//Target MUST be initialized by subclasses
+	state = IDLE;
+	position[0] = 0;
+	position[1] = 0;
+	target[0] = 0;
+	target[1] = 1;
 }
 
-void Enemy::ChasePLayer(Type t)
+void Enemy::SetState(tState s)
 {
+	state = s;
+}
 
+tState Enemy::GetState() const
+{
+	return state;
 }
 
 Enemy::~Enemy()
 {
 	//No pointers for now -> Empty
+}
+
+RedGhost::RedGhost(int posX, int posY, int targetX, int targetY)
+{
+	SetPosition(posX, posY);
+	SetTarget(targetX, targetY);
+}
+
+PinkGhost::PinkGhost(int posX, int posY, int targetX, int targetY)
+{
+	SetPosition(posX, posY);
+	SetTarget(targetX, targetY);
+}
+
+OrangeGhost::OrangeGhost(int posX, int posY, int targetX, int targetY)
+{
+	SetPosition(posX, posY);
+	SetTarget(targetX, targetY);
+}
+
+BlueGhost::BlueGhost(int posX, int posY, int targetX, int targetY)
+{
+	SetPosition(posX, posY);
+	SetTarget(targetX, targetY);
 }
