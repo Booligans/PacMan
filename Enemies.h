8e9510240186491a3abb616910162c16530e53a8
tito-kimbo <eduriver@ucm.es>
2017-05-04 10:50:38 +0200

    Update Enemies.h

--- Enemies.h
+++ Enemies.h
@@ -1,19 +1,71 @@
 #ifndef ENEMIES_H
 #define ENEMIES_H
 
-typedef enum Type{Red_Ghost, Pink_Ghost, Orange_Ghost, Scared_Ghost};
+typedef enum tState { IDLE, SCATTER, CHASE, FRIGHTENED };
 
 class Enemy
 {
 	public:
 		//Constructors
 		Enemy();
+
 		//Methods
-		void ChasePLayer(Type t);
+		void SetState(tState s);
+		void SetPosition(int posX, int posY);
+		void SetTarget(int targetX, int targetY);
+		void MoveToTarget();
+
+		tState GetState() const;
+
 		//Destructor
 		~Enemy();
 	private:
-		Type type;
+		tState state;
+	protected:
+		int target[2];
+		int position[2];
+};
+
+class RedGhost : Enemy
+{
+	public:
+		RedGhost(int posX, int posY, int targetX, int targetY);
+
+		~RedGhost();
+	private:
+		int redScatterTarget[2];
+
+};
+
+class PinkGhost : Enemy
+{
+	public:
+		PinkGhost(int posX, int posY, int targetX, int targetY);
+
+		~PinkGhost();
+	private:
+		int pinkScatterTarget[2];
+};
+
+class OrangeGhost : Enemy
+{
+	public:
+		OrangeGhost(int posX, int posY, int targetX, int targetY);
+
+		~OrangeGhost();
+	private:
+		int orangeScatterTarget[2];
+
+};
+
+class BlueGhost : Enemy
+{
+	public:
+		BlueGhost(int posX, int posY, int targetX, int targetY);
+
+		~BlueGhost();
+	private:
+		int blueScatterTarget[2];
 };
 
 //Here go the external functions needed 
