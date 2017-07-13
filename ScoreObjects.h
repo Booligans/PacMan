b8affebb1cd19c166baf419a8a07feda7700e2be
Jorgitou98 <georgeatletico@gmail.com>
2017-07-13 15:02:24 +0200

    Update ScoreObjects.h

--- ScoreObjects.h
+++ ScoreObjects.h
@@ -1,17 +1,73 @@
 #ifndef SCOREOBJECTS_H
 #define SCOREOBJECTS_H
+#include <string>
+
+struct playerData
+{
+	std::string name;
+	Score score;
+};
 
 class Score {
-	public:
-		Score();
-		
-		void IncreaseScore(const int & value);
+public:
+	Score();
 
-		const int ReturnScore();
+	void SetScore(const long long int & value);
+	void IncreaseScore(const long long int & value);
 
-		~Score();
-	private:
-		long long int score;
+	int ReturnScore() const;
+
+	~Score();
+private:
+	long long int score;
+};
+
+class ScoreObject
+{
+public:
+	ScoreObject();
+
+	~ScoreObject();
+
+protected:
+	int value;
+};
+
+class Pellet : ScoreObject
+{
+public:
+	Pellet();
+
+	void Caught(Score s);
+
+	~Pellet();
+private:
+	int value = 5;
+};
+
+class Bonus : ScoreObject
+{
+public:
+	Bonus();
+
+	void Caught(Score s);
+
+	~Bonus();
+private:
+	int value = 150;
+};
+
+
+class PowerUp : ScoreObject
+{
+public:
+	PowerUp();
+
+	void Caught(Score s);
+
+	~PowerUp();
+private:
+	int value = 50;
 };
 
 #endif
