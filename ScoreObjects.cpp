c9bf8294367cc804b862cc9ceadfc77a911beacd
Jorgitou98 <georgeatletico@gmail.com>
2017-07-13 15:07:01 +0200

    Update ScoreObjects.cpp

--- ScoreObjects.cpp
+++ ScoreObjects.cpp
@@ -1,1 +1,74 @@
 #include "ScoreObjects.h"
+
+Score::Score()
+{}
+
+void Score::SetScore(const long long int & value)
+{
+	score = value;
+}
+
+void Score::IncreaseScore(const long long int & value)
+{
+	score += value;
+}
+
+int Score::ReturnScore() const
+{
+	return score;
+}
+
+Score::~Score()
+{}
+
+ScoreObject::ScoreObject()
+{}
+ScoreObject::~ScoreObject()
+{}
+
+Pellet::Pellet()
+{
+	ScoreObject();
+}
+
+void Pellet::Caught(Score s)
+{
+	s.IncreaseScore(value);
+	Pellet::~Pellet();
+}
+
+Pellet::~Pellet()
+{
+	ScoreObject::~ScoreObject();
+}
+
+Bonus::Bonus()
+{
+	ScoreObject();
+}
+
+void Bonus::Caught(Score s)
+{
+	s.IncreaseScore(value);
+	Bonus::~Bonus();
+}
+
+Bonus::~Bonus()
+{
+	ScoreObject::~ScoreObject();
+}
+
+PowerUp::PowerUp()
+{
+
+}
+
+void PowerUp::Caught(Score s)
+{
+
+}
+
+PowerUp::~PowerUp()
+{
+
+}
