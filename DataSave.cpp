d4497b993d3a293e18ffb34e8aacf9f7d17fbbd3
Jorgitou98 <georgeatletico@gmail.com>
2017-07-14 23:29:33 +0200

    Update DataSave.cpp

--- DataSave.cpp
+++ DataSave.cpp
@@ -12,50 +12,9 @@
 	return 0;
 }*/
 
-void DecryptVigenereFile( std::string & d) {
-	std::vector <int> v;
-	int keySize = ENCRYPTION_KEY.size();
-
-	for (int i = 0; i < d.size(); ++i) {
-		v.push_back(ENCRYPTION_KEY[i % keySize] % RANGE);
-	}
-
-	for (int i = 0; i < d.size(); ++i) {
-		d[i] = d[i] - v[i];
-		if (d[i] < MIN)
-		{
-			d[i] = MAX - (MIN - d[i]);
-		}
-	}
-}
 
 
 
-std::string EncryptFile(std::string const& f) {
-
-	std::string aux2 = "";
-	int i = 0, keySize = ENCRYPTION_KEY.size();
-	char aux;
-	std::vector <int> v;
-	std::vector <char> u;
-
-		file.get(aux);
-		while (!file.eof())
-		{
-			u.push_back(aux);
-			v.push_back(ENCRYPTION_KEY[i % keySize] % RANGE);
-			file.get(aux);
-			++i;
-		}
-		NewEncryptedValues(u, v);
-		ShowNewCode(u);
-		for (int i = 0; i < u.size(); ++i) {
-			aux2 += u[i];
-		}
-		file.close();
-	
-	return aux2;
-}
 
 // 0 no pasa nada
 // 1 no encontrado archivo de carga de nivel
@@ -75,7 +34,7 @@ int LoadAndDecryptFile(std::vector <playerData> & v) {
 	else {
 		while (!f.eof()) {
 			getline(f, s);
-			DecryptVigenereFile(s);
+			DecryptVigenere(s);
 			o.str(s);
 			o >> aux.name;
 			o >> s;
@@ -89,10 +48,29 @@ int LoadAndDecryptFile(std::vector <playerData> & v) {
 }
 
 
+void DecryptVigenere(std::string & d) {
+	std::vector <int> v;
+	int keySize = ENCRYPTION_KEY.size();
+
+	for (int i = 0; i < d.size(); ++i) {
+		v.push_back(ENCRYPTION_KEY[i % keySize] % RANGE);
+	}
+
+	for (int i = 0; i < d.size(); ++i) {
+		d[i] = d[i] - v[i];
+		if (d[i] < MIN)
+		{
+			d[i] = MAX - (MIN - d[i]);
+		}
+	}
+}
+
 void SaveAndEncrypt(std::vector <playerData> const& v) {
 	std::string aux;
+	std::ofstream file;
 	std::vector <int> u;
 	int p;
+	file.open(PLAYER_INFO); // Lo queremos guardar en la carpeta PLAYER_DATAPATH = "/players/"
 	for (int i = 0; i < v.size(); ++i) {
 		aux = v[i].name + " " + std::to_string(v[i].score.ReturnScore);
 		p = 0;
@@ -102,7 +80,9 @@ void SaveAndEncrypt(std::vector <playerData> const& v) {
 			if (aux[j] > MAX) aux[j] = MIN + (aux[j] - MAX);
 			p = (p + 1) % ENCRYPTION_KEY.size();
 		}
+		file << aux << '\n';
 	}
-
-
+	file.close();
 }
+
+
